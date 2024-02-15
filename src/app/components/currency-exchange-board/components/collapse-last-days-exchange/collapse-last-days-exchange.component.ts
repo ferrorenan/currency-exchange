7bcc66735cebcdc716a59a1549ba36cf53cd70fe
Renan Ferro <renan.ferro@fmd.ag>
2024-02-14 21:48:34 -0300

    feat(estrutura): implementa estrutura para store em collapse-last-days-exchange.component

--- src/app/components/currency-exchange-board/components/collapse-last-days-exchange/collapse-last-days-exchange.component.ts
+++ src/app/components/currency-exchange-board/components/collapse-last-days-exchange/collapse-last-days-exchange.component.ts
@@ -1,6 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {Observable} from "rxjs";
 import {LatestDailyExchangeResponse} from "../../../../models/latest-daily-exchange-response";
+import {Select} from "@ngxs/store";
+import {CurrentExchangeStates} from "../../../../store/state/current-exchange-rate.state";
+import {ExchangeRateNowResponse} from "../../../../models/exchange-rate-now-response";
+import {LastDaysExchangeStates} from "../../../../store/state/last-days-exchange-rate.state";
 
 @Component({
   selector: 'app-collapse-last-days-exchange',
@@ -9,5 +13,6 @@ import {LatestDailyExchangeResponse} from "../../../../models/latest-daily-excha
 })
 export class CollapseLastDaysExchangeComponent {
 
-  @Input({required: true}) lastDaysPayload: Observable<LatestDailyExchangeResponse>;
+  @Select(LastDaysExchangeStates.getLastDaysExchangeRate) lastDaysExchangePayload$: Observable<LatestDailyExchangeResponse>;
+  @Select(LastDaysExchangeStates.hasError) hasError$: Observable<boolean>;
 }
