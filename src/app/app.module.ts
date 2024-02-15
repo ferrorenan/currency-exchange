592c18975ddb26cba477fb8dba64c9bd3852cb1a
Renan Ferro <renan.ferro@fmd.ag>
2024-02-14 22:28:37 -0300

    feat(modulos/componentes): adiciona imports de modulos/componentes para o projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgOptimizedImage} from "@angular/common";
+import {HeaderComponent} from "./components/header/header.component";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {SetDomainInterceptor} from "./interceptors/set-domain.interceptor";
+import {FooterComponent} from "./components/footer/footer.component";
+import {CurrentExchangeStates} from "./store/state/current-exchange-rate.state";
+import { NgxsModule } from '@ngxs/store';
+import {LastDaysExchangeStates} from "./store/state/last-days-exchange-rate.state";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgOptimizedImage,
+    HeaderComponent,
+    HttpClientModule,
+    FooterComponent,
+    NgxsModule.forRoot([
+      CurrentExchangeStates,
+      LastDaysExchangeStates
+    ], {
+      selectorOptions: {
+        suppressErrors: false,
+        injectContainerState: false
+      }
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: SetDomainInterceptor,
+      multi: true,
+    },
+  ],
+
 })
-export class AppModule { }
+export class AppModule {
+}
