9b8156547a315df41620c5dca96c9500a7c6b02b
Renan Ferro <renan.ferro@fmd.ag>
2024-02-14 22:28:04 -0300

    feat(rota): adiciona estrutura de rota para home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/home/home.module')
+      .then(module => module.HomeModule),
+    title: 'BRL Exchange Rate',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
